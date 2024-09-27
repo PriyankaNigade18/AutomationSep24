35631ab830bb4db0e2f2514ad8a20085a9fb463e
PriyankaN <piyu1818@gmail.com>
2024-09-27 20:28:58 +0530

    27-09-2024 WebDriver Day1

---
+++ src/test/java/com/webTesting/FirstBrowserTest.java
@@ -0,0 +1,16 @@
+package com.webTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstBrowserTest {
+
+	public static void main(String[] args)
+	{
+		// Create a driver session
+		WebDriver driver=new ChromeDriver();
+		
+
+	}
+
+}
---
+++ src/test/java/com/webTesting/GoogleTitleValidation.java
@@ -0,0 +1,52 @@
+package com.webTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTitleValidation {
+
+	public static void main(String[] args)
+	{
+		//Create a driver session
+		WebDriver driver=new ChromeDriver();
+		
+		//open application
+		driver.get("https://www.google.com");
+		
+		//get the title
+		String actTitle=driver.getTitle();
+		
+		//compare with expected
+		String expTitle="Google";
+		if(actTitle.equals(expTitle))
+		{
+			System.out.println("Title matched....Test Pass!");
+		}else
+		{
+			System.out.println("Title not matched...Test Fail");
+			
+		}
+		
+		
+		//close the browser close() quit()
+		
+		driver.quit();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
